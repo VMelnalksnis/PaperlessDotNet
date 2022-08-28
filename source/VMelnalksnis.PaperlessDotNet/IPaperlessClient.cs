a62c7b32e569ad030141cb190a95afba86fc2768
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-08-28 13:47:59 +0300

    feat: Setup project

---
+++ source/VMelnalksnis.PaperlessDotNet/IPaperlessClient.cs
@@ -0,0 +1,10 @@
+﻿// Copyright 2022 Valters Melnalksnis
+// Licensed under the Apache License 2.0.
+// See LICENSE file in the project root for full license information.
+
+namespace VMelnalksnis.PaperlessDotNet;
+
+/// <summary>All available Paperless APIs.</summary>
+public interface IPaperlessClient
+{
+}
