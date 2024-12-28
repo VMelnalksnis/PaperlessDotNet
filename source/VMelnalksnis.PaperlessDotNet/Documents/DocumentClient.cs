dd23846127c67336dc4a967faae8514f44629401
Sean <noyb@noyb.com>
2024-12-28 13:15:30 +0100

    sig: added cpg signature doc: wording change in documentation

--- source/VMelnalksnis.PaperlessDotNet/Documents/DocumentClient.cs
+++ source/VMelnalksnis.PaperlessDotNet/Documents/DocumentClient.cs
@@ -93,7 +93,7 @@ public sealed class DocumentClient : IDocumentClient
 	/// <summary>
 	/// Gets all documents tagged with a certain tag Id.
 	/// </summary>
-	/// <param name="tagId">Id of the tag for which to retrieve all documents.</param>
+	/// <param name="tagId">Id of a tag for which to retrieve all documents.</param>
 	/// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
 	/// <returns>An enumerable which will asynchronously iterate over all retrieved documents.</returns>
 	public IAsyncEnumerable<Document> GetAllByTagId(int tagId, CancellationToken cancellationToken = default)
